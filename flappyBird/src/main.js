ad6580c6e6c05382b193e79ea1bd65ef08d765ad
iamnabil <23001886@siswa.um.edu.my>
2025-08-08 22:35:11 +0800

    add scene

--- flappyBird/src/main.js
+++ flappyBird/src/main.js
@@ -1,10 +1,44 @@
 import kaplay from "kaplay";
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+const k = kaplay({
+ 
+});
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("base", "sprites/base.png");
+k.loadSprite("bg", "sprites/bg.png");
+k.loadSprite("pipe", "sprites/pipe.png");
+
+
+
+k.scene("background", () => {
+    k.add([
+        k.sprite("bg", {width : width() , height : height()}),
+        k.pos(0,0),
+        k.fixed(),
+    ]);
+    k.add([
+        k.sprite("pipe"),
+        k.pos(width() - 100 , 500),
+
+    ]);
+     k.add([
+        k.sprite("pipe", {flipY: true}),
+        k.pos(width() - 100 , -50),
+
+    ]);
+    k.add([
+    k.pos(0,620),
+    k.sprite("base",{width : width(), height : 120}),
+    area(),
+    body({
+        isStatic : true
+    })
+    ]);
+});
+k.go("background");
 
 let idle = true;
 const bean = k.add([
